fe2a4542535f327643835995ef808902289a4ada
Haruto0307 <skybolt0307@gmail.com>
2014-01-05 16:37:43 +0900

    Save an in_array call and slightly improve readability.

--- src/Mcamara/LaravelLocalization/LaravelLocalization.php
+++ src/Mcamara/LaravelLocalization/LaravelLocalization.php
@@ -5,7 +5,6 @@ use Illuminate\View\Environment;
 use Illuminate\Translation\Translator;
 use Request;
 use Session;
-use Cookie;
 use App;
 use View;
 use Config;
@@ -110,15 +109,7 @@ class LaravelLocalization
 		{
 			Session::put('language', $this->currentLanguage);
 		}
-        if($this->configRepository->get('laravel-localization::useCookieLanguage'))
-        {
-            Cookie::queue(Cookie::forever('language', $this->currentLanguage));
-        }
-        //Forget the language cookie if it's disabled and exists
-        else if (Cookie::get('language') != null)
-        {
-            Cookie::forget('language');
-        }
+
 		return $locale;
 	}
 
@@ -249,7 +240,7 @@ class LaravelLocalization
 			$route = url($language."/".$translation);
 			if(is_array($attributes))
 			{
-				foreach ($attributes as $key => $value)
+				foreach ($attributes as $key => $value) 
 				{
 					$route = str_replace("{".$key."}", $value, $route);
 					$route = str_replace("{".$key."?}", $value, $route);
@@ -355,21 +346,18 @@ class LaravelLocalization
 		{
 			return Session::get('language');
 		}
-        // or get cookie language...
-        else if($this->configRepository->get('laravel-localization::useCookieLanguage') &&
-            Cookie::get('language') != null &&
-            in_array(Cookie::get('language'), $languages))
-        {
-            return Cookie::get('language');
-        }
 		// or get browser language...
-		else if($this->configRepository->get('laravel-localization::useBrowserLanguage'))
+		else if($this->configRepository->get('laravel-localization::useBrowserLanguage') &&
+					isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) &&
+					in_array(substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2), $languages))
 		{
-			return $this->negotiateLanguage();
+			return substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
 		}
-
 		// or get application default language
-		return $this->configRepository->get('app.locale');
+		else
+		{
+			return $this->configRepository->get('app.locale');
+		}
 	}
 
 	/**
@@ -425,94 +413,6 @@ class LaravelLocalization
 	    return false;
 	}
 
-    /**
-     * Negotiates language with the user's browser through the Accept-Language
-     * HTTP header or the user's host address.  Language codes are generally in
-     * the form "ll" for a language spoken in only one country, or "ll-CC" for a
-     * language spoken in a particular country.  For example, U.S. English is
-     * "en-US", while British English is "en-UK".  Portugese as spoken in
-     * Portugal is "pt-PT", while Brazilian Portugese is "pt-BR".
-     *
-     * This function is based on negotiateLanguage from Pear HTTP2
-     * http://pear.php.net/package/HTTP2/
-     *
-     * Quality factors in the Accept-Language: header are supported, e.g.:
-     *      Accept-Language: en-UK;q=0.7, en-US;q=0.6, no, dk;q=0.8
-     *
-     * @return string  The negotiated language result or app.locale.
-     */
-    public function negotiateLanguage()
-    {
-        $default = $this->configRepository->get('app.locale');
-        $supported = array();
-        foreach ($this->configRepository->get('laravel-localization::languagesAllowed') as $lang) {
-            $supported[strtolower($lang)] = $lang;
-        }
-
-        if (!count($supported)) {
-            return $default;
-        }
-
-        if (Request::header('Accept-Language')) {
-            $matches = array();
-            $generic_matches = array();
-            foreach (explode(',', Request::header('Accept-Language')) as $option) {
-                $option = array_map('trim', explode(';', $option));
-
-                $l = strtolower($option[0]);
-                if (isset($option[1])) {
-                    $q = (float) str_replace('q=', '', $option[1]);
-                } else {
-                    $q = null;
-                    // Assign default low weight for generic values
-                    if ($l == '*/*') {
-                        $q = 0.01;
-                    } elseif (substr($l, -1) == '*') {
-                        $q = 0.02;
-                    }
-                }
-                // Unweighted values, get high weight by their position in the
-                // list
-                $q = isset($q) ? $q : 1000 - count($matches);
-                $matches[$l] = $q;
-
-                //If for some reason the Accept-Language header only sends language with country
-                //we should make the language without country an accepted option, with a value
-                //less than it's parent.
-                $l_ops = explode('-', $l);
-                array_pop($l_ops);
-                while (!empty($l_ops)) {
-                    //The new generic option needs to be slightly less important than it's base
-                    $q -= 0.001;
-                    $generic_matches[implode('-', $l_ops)] = $q;
-                    array_pop($l_ops);
-                }
-            }
-            $matches = array_merge($generic_matches, $matches);
-
-            arsort($matches, SORT_NUMERIC);
-
-            foreach ($matches as $key => $q) {
-                if (isset($supported[$key])) {
-                    return $supported[$key];
-                }
-            }
-            // If any (i.e. "*") is acceptable, return the first supported format
-            if (isset($matches['*'])) {
-                return array_shift($supported);
-            }
-        }
-
-        if (Request::server('REMOTE_HOST')) {
-            $lang = strtolower(end($h = explode('.', Request::server('REMOTE_HOST'))));
-            if (isset($supported[$lang])) {
-                return $supported[$lang];
-            }
-        }
-
-        return $default;
-    }
-
 }
 
 Route::filter('LaravelLocalizationRedirectFilter', function()
@@ -525,15 +425,14 @@ Route::filter('LaravelLocalizationRedirectFilter', function()
 		$language = $params[0];
 		$languages = Config::get('laravel-localization::languagesAllowed');
 
-		if(in_array($language, $languages) && $language === $defaultLanguage &&
-			Config::get('laravel-localization::hideDefaultLanguageInRoute') )
-		{
-			return Redirect::to($app['laravellocalization']->getCleanRoute(), 301);
-		}
-
-		if(!in_array($language, $languages) && 
-			($currentLanguage !== $defaultLanguage || !Config::get('laravel-localization::hideDefaultLanguageInRoute'))
-		)
+		if (in_array($language, $languages))
+        {
+            if ($language === $defaultLanguage && Config::get('laravel-localization::hideDefaultLanguageInRoute'))
+            {
+                return Redirect::to($app['laravellocalization']->getCleanRoute(), 301);
+            }
+        }
+		else if ($currentLanguage !== $defaultLanguage || !Config::get('laravel-localization::hideDefaultLanguageInRoute'))
 		{
 			// If the current url does not contain any language
 			// The system redirect the user to the very same url "languaged"
