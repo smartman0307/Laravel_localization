038dae6167315d47d8d7a6c65242705270d11be5
Haruto0307 <skybolt0307@gmail.com>
2015-01-30 16:42:52 +0000

    Fixed cookies, still have to fix tests

--- src/Mcamara/LaravelLocalization/LaravelLocalizationServiceProvider.php
+++ src/Mcamara/LaravelLocalization/LaravelLocalizationServiceProvider.php
@@ -8,103 +8,66 @@ use Session;
 
 class LaravelLocalizationServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('mcamara/laravel-localization');
-	}
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-        $app = $this->app;
-        $app->router->filter('LaravelLocalizationRedirectFilter', function() use($app)
-        {
-            $currentLocale = $app['laravellocalization']->getCurrentLocale();
-            $defaultLocale = $app['laravellocalization']->getDefault();
-            $params = explode('/', Request::path());
-            if (count($params) > 0)
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return ['modules.handler', 'modules'];
+    }
+    /**
+     * Register the package resources.
+     *
+     * @return void
+     */
+    protected function registerResources()
+    {
+        $userConfigFile    = app()->configPath().'/laravel-localization/config.php';
+        $packageConfigFile = __DIR__ . '/../../config/config.php';
+        $config            = $this->app['files']->getRequire($packageConfigFile);
+
+        if (file_exists($userConfigFile)) {
+            $userConfig = $this->app['files']->getRequire($userConfigFile);
+            $config     = array_replace_recursive($config, $userConfig);
+        }
+
+        $this->app['config']->set('laravel-localization', $config);
+    }
+
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->registerResources();
+
+        $this->app[ 'laravellocalization' ] = $this->app->share(
+            function ()
             {
-                $localeCode = $params[0];
-                $locales = $app['laravellocalization']->getSupportedLocales();
-                $hideDefaultLocale = $app['laravellocalization']->hideDefaultLocaleInURL();
-                $redirection = false;
-                
-                if (!empty($locales[$localeCode]))
-                {
-                    if ($localeCode === $defaultLocale && $hideDefaultLocale)
-                    {
-                        $redirection = $app['laravellocalization']->getNonLocalizedURL();
-                    }
-                }
-                else if ($currentLocale !== $defaultLocale || !$hideDefaultLocale)
-                {
-                    // If the current url does not contain any locale
-                    // The system redirect the user to the very same url "localized"
-                    // we use the current locale to redirect him
-                    $redirection = $app['laravellocalization']->getLocalizedURL();
-                }
-                    
-                if($redirection)
-                {
-                    // Save any flashed data for redirect
-                    Session::reflash();
-                    return Redirect::to($redirection, 307)->header('Vary','Accept-Language');
-                }
+                return new LaravelLocalization();
             }
-        });
-
-        /**
-         * 	This filter would set the translated route name
-         */
-        $app->router->filter('LaravelLocalizationRoutes', function()
-        {
-            $app = $this->app;
-            $routeName = $app['laravellocalization']->getRouteNameFromAPath($app['router']->current()->uri());
-
-            $app['laravellocalization']->setRouteName($routeName);
-            return;
-        });
-
-		$app['config']->package('mcamara/laravel-localization', __DIR__.'/../config');
-
-		$app['laravellocalization'] = $app->share(
-            function() use($app)
-    		{
-    			return new LaravelLocalization(
-                    $app['config'], 
-                    $app['view'], 
-                    $app['translator'], 
-                    $app['router'], 
-                    $app
-                );
-    		}
         );
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    }
 
 }
